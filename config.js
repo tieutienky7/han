03972a2838992cfb8399ad5f811e2163e2293178
tieutienky7 <thich9x.nh@gmail.com>
2021-10-21 21:59:17 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐Chào cái Hân🖐',
+    introDesc: `Anh muốn hỏi em một lần nữa, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Anh hỏi đi',
+    title: 'Em có thương anh không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thương Anh đó nha :> ',
+    btnYes: 'Có, em thương anh 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết vì sao Em thương anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì em muốn cưới anh....... 😊😊',
+    mess: 'Sinh nhật em...Mong cho cuộc sống của em ngày nào cũng vui vẻ và hạnh phúc như ngày tốt nghiệp của em! 😘😘',
+    messDesc: 'Thích iu Hân!',
+    btnAccept: 'muốn chụp với e một tấm hình, tấm hình có 3 người ! 💖',
+    messLink: 'https://www.messenger.com/t/100010410543420/'
+}
